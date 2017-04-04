a852e86fdca4d3fca9688ae5be499115940d4246
Liczi <jak.liczner@gmail.com>
2017-04-05 01:08:44 +0200

    Initial commit

---
+++ src/main/java/si/csp/Main.java
@@ -0,0 +1,13 @@
+package si.csp;
+
+
+import si.csp.utils.Drawer;
+
+public class Main {
+
+
+    static public void main(String args[]) {
+//        Drawer drawer = new Drawer();
+//        drawer.drawGraph();
+    }
+}
